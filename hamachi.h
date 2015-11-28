184453ab2c65bb098089843a995e4c305c04dba6
Mike Roup <roup.michael@gmail.com>
2015-11-28 16:01:44 -0500

    add hamachi

---
+++ hamachi.h
@@ -0,0 +1,10 @@
+#ifndef _HAMACHI_
+#define _HAMACHI_
+
+#define SWAP_ON(t,x,y) do {                     \
+    t = x;                                      \
+    x = y;                                      \
+    y = t;                                      \
+  } while (0)
+
+#endif
